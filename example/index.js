45977ed1f065dbe7d8e7a03ecac9144ffc305f4b
sugarshin <shinsugar@gmail.com>
2017-08-14 22:10:47 +0900

    Add MessagePanel

--- example/index.js
+++ example/index.js
@@ -1,26 +1,25 @@
 import 'normalize.css/normalize.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import App from './App';
 
 const main = () => {
-  const meta = document.createElement('meta')
-  meta.setAttribute('name', 'viewport')
-  meta.setAttribute('content', 'width=device-width, initial-scale=1')
-  document.head.appendChild(meta)
+  const meta = document.createElement('meta');
+  meta.setAttribute('name', 'viewport');
+  meta.setAttribute('content', 'width=device-width, initial-scale=1');
+  document.head.appendChild(meta);
 
-  const root = document.body.appendChild(document.createElement('div'))
-  ReactDOM.render(<AppContainer><App /></AppContainer> , root)
+  const root = document.body.appendChild(document.createElement('div'));
+  ReactDOM.render(<AppContainer><App /></AppContainer> , root);
 
   if (module.hot) {
     module.hot.accept('./App', () => {
-      const NextApp = require('./App').default
-      ReactDOM.render(<AppContainer><NextApp /></AppContainer>, root)
-    })
+      const NextApp = require('./App').default;
+      ReactDOM.render(<AppContainer><NextApp /></AppContainer>, root);
+    });
   }
-}
+};
 
-main()
+main();
